f7b365746c58525b8ddf0fded4466f00903bb4fa
Frederic BIDON <fredbi@yahoo.com>
2023-02-02 14:19:35 +0100

    chore(gitregostore): added minimal linting rules enforcement
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- gitregostore/gitstoremethods_test.go
+++ gitregostore/gitstoremethods_test.go
@@ -3,139 +3,212 @@ package gitregostore
 import (
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func gs_tests(t *testing.T, gs *GitRegoStore) {
-
 	index := 0
 
-	// Rules
-	policies, err := gs.GetOPAPolicies()
-	if err != nil || policies == nil {
-		t.Errorf("failed to get all policies %v", err)
-	}
-	policiesNames, err := gs.GetOPAPoliciesNamesList()
-	if err != nil || len(policiesNames) == 0 {
-		t.Errorf("failed to get policies names list %v", err)
-		return
-	}
-	policy, err := gs.GetOPAPolicyByName(policiesNames[index])
-	if err != nil || policy == nil {
-		t.Errorf("failed to get policy by name: '%s', %v", policiesNames[index], err)
-	}
-	// Controls
-	controls, err := gs.GetOPAControls()
-	if err != nil || controls == nil {
-		t.Errorf("failed to get all controls %v", err)
-	}
-	controlsNames, err := gs.GetOPAControlsNamesList()
-	if err != nil || len(controlsNames) == 0 {
-		t.Errorf("failed to get controls names list %v", err)
-		return
-	}
+	t.Run("should retrieve OPA rules (policies)", func(t *testing.T) {
+		t.Parallel()
 
-	control, err := gs.GetOPAControlByName(controlsNames[index])
-	if err != nil || control == nil {
-		t.Errorf("failed to get control by name: '%s', %v", controlsNames[index], err)
-	}
-	controlsIDs, err := gs.GetOPAControlsIDsList()
-	if err != nil || len(controlsIDs) == 0 {
-		t.Errorf("failed to get controls ids list %v", err)
-		return
-	}
+		policies, err := gs.GetOPAPolicies()
+		assert.NoError(t, err)
+		assert.NotNilf(t, policies,
+			"failed to get all policies %v", err,
+		)
+	})
 
-	control, err = gs.GetOPAControlByID(controlsIDs[index])
-	if err != nil || control == nil {
-		t.Errorf("failed to get control by ID: '%s', %v", controlsNames[index], err)
-	}
-	// Frameworks
-	frameworks, err := gs.GetOPAFrameworks()
-	if err != nil || frameworks == nil {
-		t.Errorf("failed to get all frameworks %v", err)
-	}
-	frameworksNames, err := gs.GetOPAFrameworksNamesList()
-	if err != nil || len(frameworksNames) == 0 {
-		t.Errorf("failed to get frameworks names list %v", err)
-		return
-	}
-	framework, err := gs.GetOPAFrameworkByName(frameworksNames[0])
-	if err != nil || framework == nil {
-		t.Errorf("failed to get framework by name: '%s', %v", frameworksNames[0], err)
-	}
+	t.Run("should retrieve OPA policy names", func(t *testing.T) {
+		t.Parallel()
 
-	if len(framework.Controls) == 0 {
-		t.Errorf("failed to get controls for framework name: '%s'", framework.Name)
-	}
+		policiesNames, err := gs.GetOPAPoliciesNamesList()
+		require.NoError(t, err)
+		require.NotEmptyf(t, policiesNames,
+			"failed to get policies names list %v", err,
+		)
 
-	if len(*framework.ControlsIDs) == 0 {
-		t.Errorf("failed to get controls ids for framework name: '%s'", framework.Name)
-	}
+		require.Greater(t, len(policiesNames), index)
 
-	for i := range framework.Controls {
-		if len(framework.Controls[i].Rules) == 0 {
-			t.Errorf("failed to get rules for framework name: '%s', control name: '%s'", framework.Name, framework.Controls[i].Name)
-		}
+		t.Run("should retrieve OPA policy by name", func(t *testing.T) {
+			t.Parallel()
 
-		if len(*framework.ControlsIDs) == 0 {
-			t.Errorf("failed to get rules ids for framework name: '%s', control name: '%s'", framework.Name, framework.Controls[i].Name)
-		}
+			policy, err := gs.GetOPAPolicyByName(policiesNames[index])
+			assert.NoError(t, err)
+			assert.NotNilf(t, policy,
+				"failed to get policy by name: '%s', %v", policiesNames[index], err,
+			)
+		})
+	})
 
-	}
+	t.Run("should retrieve OPA controls", func(t *testing.T) {
+		t.Parallel()
 
-	control, err = gs.GetOPAControlByFrameworkNameAndControlName("NSA", "Allow privilege escalation")
+		controls, err := gs.GetOPAControls()
+		assert.NoError(t, err)
+		assert.NotEmptyf(t, controls,
+			"failed to get all controls %v", err,
+		)
+	})
 
-	if err != nil || control == nil {
-		t.Errorf("failed to get control by framework name 'NSA' and control name 'Allow privilege escalation': %v", err)
-	} else {
-		if strings.ToLower(control.ControlID) != "c-0016" {
-			t.Errorf("wrong control for framework name 'NSA' and control name 'Allow privilege escalation' expected: 'C-0016', found %s", control.ControlID)
-		}
-	}
+	t.Run("should retrieve OPA controls names", func(t *testing.T) {
+		t.Parallel()
 
+		controlsNames, err := gs.GetOPAControlsNamesList()
+		require.NoError(t, err)
+		require.NotEmptyf(t, controlsNames,
+			"failed to get controls names list %v", err,
+		)
+
+		require.Greater(t, len(controlsNames), index)
+
+		t.Run("should retrieve OPA control by name", func(t *testing.T) {
+			t.Parallel()
+
+			control, err := gs.GetOPAControlByName(controlsNames[index])
+			assert.NoError(t, err)
+			assert.NotNilf(t, control,
+				"failed to get control by name: '%s', %v", controlsNames[index], err,
+			)
+		})
+	})
+
+	t.Run("should retrieve OPA controls IDs", func(t *testing.T) {
+		t.Parallel()
+
+		controlsIDs, err := gs.GetOPAControlsIDsList()
+		require.NoError(t, err)
+		require.NotEmptyf(t, controlsIDs,
+			"failed to get controls ids list %v", err,
+		)
+
+		require.Greater(t, len(controlsIDs), index)
+
+		t.Run("should retrieve OPA control by ID", func(t *testing.T) {
+			t.Parallel()
+
+			control, err := gs.GetOPAControlByID(controlsIDs[index])
+			assert.NoError(t, err)
+			assert.NotNilf(t, control,
+				"failed to get control by ID: '%s', %v", controlsIDs[index], err,
+			)
+		})
+	})
+
+	t.Run("should retrieve OPA frameworks", func(t *testing.T) {
+		t.Parallel()
+
+		frameworks, err := gs.GetOPAFrameworks()
+		assert.NoError(t, err)
+		assert.NotEmptyf(t, frameworks,
+			"failed to get all frameworks %v", err,
+		)
+	})
+
+	t.Run("should retrieve OPA frameworks names", func(t *testing.T) {
+		t.Parallel()
+
+		frameworksNames, err := gs.GetOPAFrameworksNamesList()
+		require.NoError(t, err)
+		require.NotEmptyf(t, frameworksNames,
+			"failed to get frameworks names list %v", err,
+		)
+
+		t.Run("should retrieve OPA framework by name", func(t *testing.T) {
+			t.Parallel()
+
+			framework, err := gs.GetOPAFrameworkByName(frameworksNames[0])
+			assert.NoError(t, err)
+			assert.NotNilf(t, framework,
+				"failed to get framework by name: '%s', %v", frameworksNames[0], err,
+			)
+
+			assert.NotEmptyf(t, framework.Controls,
+				"failed to get controls for framework name: '%s'", framework.Name,
+			)
+
+			require.NotNil(t, framework.ControlsIDs)
+			assert.NotEmptyf(t, *framework.ControlsIDs,
+				"failed to get controls ids for framework name: '%s'", framework.Name,
+			)
+
+			for _, control := range framework.Controls {
+				assert.NotEmptyf(t, control.Rules,
+					"failed to get rules for framework name: '%s', control name: '%s'", framework.Name, control.Name,
+				)
+			}
+		})
+	})
+
+	t.Run("should retrieve OPA control by names", func(t *testing.T) {
+		t.Parallel()
+
+		control, err := gs.GetOPAControlByFrameworkNameAndControlName("NSA", "Allow privilege escalation")
+		assert.NoError(t, err)
+		require.NotNilf(t, control,
+			"failed to get control by framework name 'NSA' and control name 'Allow privilege escalation': %v", err,
+		)
+		assert.Equalf(t, "c-0016", strings.ToLower(control.ControlID),
+			"wrong control for framework name 'NSA' and control name 'Allow privilege escalation' expected: 'C-0016', found %s", control.ControlID,
+		)
+	})
 }
 
 func TestGetPoliciesMethodsNew(t *testing.T) {
+	t.Parallel()
+
 	gs := NewDefaultGitRegoStore(-1)
-	err := gs.SetRegoObjects()
-	if err != nil {
-		t.Errorf("error in SetRegoObjects: %v", err)
-	}
+	t.Run("shoud set objects in rego store", func(t *testing.T) {
+		require.NoError(t, gs.SetRegoObjects())
+	})
+
 	gs_tests(t, gs)
 
 }
 
 func TestGetOPAFrameworkByName(t *testing.T) {
+	t.Parallel()
+
 	gs := NewDevGitRegoStore(-1)
-	err := gs.SetRegoObjects()
-	if err != nil {
-		t.Errorf("error in SetRegoObjects: %v", err)
-	}
 
-	_, err = gs.GetOPAFrameworkByName("CIS")
+	t.Run("shoud set objects in rego store", func(t *testing.T) {
+		require.NoError(t, gs.SetRegoObjects())
+	})
 
-	if err != nil {
-		t.Errorf("failed to get framework object: %v", err)
-	}
+	t.Run("shoud retrieve CIS framework", func(t *testing.T) {
+		_, err := gs.GetOPAFrameworkByName("CIS")
+		require.NoErrorf(t, err,
+			"failed to get framework object: %v", err,
+		)
+	})
 }
 
 func TestGetPoliciesMethodsOld(t *testing.T) {
+	t.Parallel()
+
 	gs := InitGitRegoStore("https://github.com", "kubescape", "regolibrary", "releases", "latest/download", "", 15)
 
 	gs_tests(t, gs)
 }
 
 func TestGetPoliciesMethodsDevNewParams(t *testing.T) {
+	t.Parallel()
+
 	gs := InitGitRegoStore("https://raw.githubusercontent.com", "kubescape", "regolibrary", "releaseDev", "/", "dev", -1)
 
 	gs_tests(t, gs)
-
 }
 
 func TestGetPoliciesMethodsDevNew(t *testing.T) {
+	t.Parallel()
+
 	gs := NewDevGitRegoStore(-1)
-	err := gs.SetRegoObjects()
-	if err != nil {
-		t.Errorf("error in SetRegoObjects: %v", err)
-	}
+
+	t.Run("should set the rego store", func(t *testing.T) {
+		require.NoError(t, gs.SetRegoObjects())
+	})
+
 	gs_tests(t, gs)
 }
