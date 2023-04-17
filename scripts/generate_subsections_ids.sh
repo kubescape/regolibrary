ad89c47c10b8e73df40b0f74f69b4880629bcdad
Yuval Leibovich <yuvall@armosec.io>
2023-04-17 19:46:16 +0300

    fixing subsections ids script + permissions
    Signed-off-by: Yuval Leibovich <yuvall@armosec.io>

--- scripts/generate_subsections_ids.sh
+++ scripts/generate_subsections_ids.sh
@@ -3,8 +3,6 @@
 #!/bin/sh
 
 frameworksVal="cis-aks-t1.2.0 cis-eks-t1.2.0 cis-v1.23-t1.0.1"
-# frameworksVal="cis-v1.23-t1.0.1"
-
 
 for val in $frameworksVal; do
     echo "Started updating framework '$val' subscections ids"
