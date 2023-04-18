d8ef25ae9c2e0f7112132f2b34c3ab23fa70b0c3
Yuval Leibovich <yuvall@armosec.io>
2023-04-18 10:09:30 +0300

    fix
    Signed-off-by: Yuval Leibovich <yuvall@armosec.io>

--- scripts/export.py
+++ scripts/export.py
@@ -407,11 +407,11 @@ def export_json(data: dict, f_name:str, output_path: str):
     os.makedirs(output_path, exist_ok=True)
     try:    
         # generate with extentions for system testing 
-        # with open(os.path.join(output_path, f"{f_name.lower()}.json"), "w") as f:
-        #     f.write(json.dumps(data, indent=4))
-        # generate without extentions for backward compatability 
-        with open(os.path.join(output_path, f"{f_name.lower()}"), "w") as f:
+        with open(os.path.join(output_path, f"{f_name.lower()}.json"), "w") as f:
             f.write(json.dumps(data, indent=4))
+        # generate without extentions for backward compatability 
+        # with open(os.path.join(output_path, f"{f_name.lower()}"), "w") as f:
+        #     f.write(json.dumps(data, indent=4))
     except Exception as e:
         logging.error(f"failed to open path: '{output_path}'")
         raise TypeError(e)
