6360493cf735bea491548cd10eb685091a5670c9
Yuval Leibovich <yuvall@armosec.io>
2023-12-14 09:11:36 +0200

    updating script + fixing the issue

--- scripts/generate_subsections_ids.py
+++ scripts/generate_subsections_ids.py
@@ -9,26 +9,39 @@ import argparse
 import json
 import os
 import re
+import logging
+import sys
+
 
 # constants
 currDir = os.path.abspath(os.getcwd())
 frameworks_dir = os.path.join(currDir, 'frameworks')
 framework_name_to_filename_mapping = {}
+logging.basicConfig(level=logging.INFO)
+
 
 # ================================================
 
 
 def init_framework_name_to_filename_mapping():
     for filename in os.listdir(frameworks_dir):
+        logging.info(f"Checking file: {filename}")
         # Load the JSON files
         if filename.endswith('.json'):
-            with open(os.path.join(frameworks_dir, filename)) as f1:
-                framework = json.load(f1)
-                framework_name_to_filename_mapping[framework['name']] = filename
+            logging.info(f"file {filename} detected as a JSON")
+            try:
+                with open(os.path.join(frameworks_dir, filename)) as f1:
+                    framework = json.load(f1)
+                    framework_name_to_filename_mapping[framework['name']] = filename
+            except Exception as e:
+                logging.error(f"Error detected with file {filename}. Error: {e}")
+                sys.exit(1)
+
 
 
 def init_parser():
     # Set up the argument parser
+    logging.info(f"Initializing parser")
     parser = argparse.ArgumentParser()
     parser.add_argument("--framework", "-fw", required=True, help="Name of the framework to add the control to")
     parser.add_argument("--firstCleanList", "-clean", required=False, help="Clean controlIds list before population")
@@ -38,13 +51,16 @@ def init_parser():
 
 
 def restart_controlIDs_list(framework):
+    logging.info(f"Restarting controls ID list")
     for subsection1 in framework["subSections"]:
         if "subSections" in framework["subSections"][subsection1]:
             for item in framework["subSections"][subsection1]["subSections"]:
                 framework["subSections"][subsection1]["subSections"][item]["controlsIDs"] = []
+    logging.info(f"Restarting controls ID completed")
 
 
 def populate_controlIds_list(framework):
+    logging.info(f"Populating controls ID list")
     for active_control in framework["activeControls"]:
         control_id = active_control["controlID"]
         cis_subsection = active_control["patch"]["name"].split(" ")[0].replace("CIS-", "")
@@ -55,11 +71,12 @@ def populate_controlIds_list(framework):
                 tmp_controlIDs.append(control_id)
 
 
-def main():
-    args = init_parser()
-    framework_name = args.framework
-    restart_controlIDs_lists = args.firstCleanList
-        
+def main(framework):
+    # args = init_parser()
+    # framework_name = args.framework
+    framework_name = framework
+    # restart_controlIDs_lists = args.firstCleanList
+    restart_controlIDs_lists = True        
 
     init_framework_name_to_filename_mapping()
 
@@ -84,5 +101,10 @@ def main():
 
 
 if __name__ == "__main__":
-    # TODO: add comments and python convetion for all document
-    main()
+    logging.info("Script started")
+    frameworks = ["cis-aks-t1.2.0", "cis-eks-t1.2.0", "cis-v1.23-t1.0.1"]
+    for i in frameworks:
+        logging.info(f"Running on framework {i}")
+        main(i)
+    logging.info("Script ended")
+    sys.exit(0)
